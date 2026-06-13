687c0e1e72aef5424b50573513203713b05f4e76
rukundok84 <rukundok84@gmail.com>
2026-06-13 20:03:29 +0000

    fixed children table issues

--- app/admin/children/page.tsx
+++ app/admin/children/page.tsx
@@ -1,10 +1,11 @@
 import { data } from "@/components/admin-sidebar";
-import { DataTable } from "@/components/data-table";
+import DataTable from "@/components/data-table";
+
 
 export default function Page() {
     return(
         <>
-            <DataTable data={data}/>
+            <DataTable/>
         </>
     )
 }
